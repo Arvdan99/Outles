112ea0ef999b815e86a02e9f298a74783cbacdac
Alumno <cesarrgomez@hotmail.com>
2022-08-16 09:40:35 -0700

    Primer commit

--- Outles/ViewController.swift
+++ Outles/ViewController.swift
@@ -9,9 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //Un OUTLET es una conexiòn entre un Control de UI del Storyboard y una variable con codigo swift
+    @IBOutlet weak var lblMiEtiqueta: UILabel!
+    
+    
+    //Esta función se ejecuta ya que cargó todo en memoria (Antes de aparecer en pantalla)
+    //Lo que pngamos aqui sobreescribe lo del Storyboard
     override func viewDidLoad() {
         super.viewDidLoad()
+        lblMiEtiqueta.text = "Este es texto desde código"
+        lblMiEtiqueta.textColor = UIColor.cyan
         // Do any additional setup after loading the view.
     }
 
